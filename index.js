7210057067a653821590296a0d90ec6d4329e5ae
ashutosh <adityatripathi1011@gmail.com>
2023-02-16 21:34:34 +0530

    first1

---
+++ index.js
@@ -0,0 +1,30 @@
+const image = document.querySelector('.slider-image');
+let counter = 0;
+
+function right(){
+  if(counter < 500){
+    counter+=100;
+    image.style.transition = `1s`;
+    image.style.transform = `translate(${-counter}%)`
+  }
+}
+
+const left = () => {
+    if(counter > 0){
+    counter-=100;
+    image.style.transition = `1s`;
+    image.style.transform = `translate(${-counter}%)`
+  }
+}
+
+
+setInterval(()=>{
+  if(counter >= 0 && counter <= 500){
+    counter += 100;
+  }
+  else{
+    counter = 0;
+  }
+  image.style.transition = `1s`;
+  image.style.transform = `translate(${-counter}%)`
+},3000);
