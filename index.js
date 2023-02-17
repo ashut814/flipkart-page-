5021427a6be14cbdc73145232e55c059d780f2f5
ashutosh <adityatripathi1011@gmail.com>
2023-02-17 16:02:02 +0530

    v2

--- index.js
+++ index.js
@@ -2,29 +2,17 @@ const image = document.querySelector('.slider-image');
 let counter = 0;
 
 const  right = () => {
-  if(counter < 500){
-    counter+=100;
+  if(counter < 6){
+    counter+=1;
     image.style.transition = `1s`;
-    image.style.transform = `translate(${-counter}%)`
+    image.style.transform = `translate(${-counter * 100}%)`
   }
 }
 
 const left = () => {
     if(counter > 0){
-    counter-=100;
+    counter-=1;
     image.style.transition = `1s`;
-    image.style.transform = `translate(${-counter}%)`
+    image.style.transform = `translate(${-counter *100}%)`
   }
 }
-
-
-setInterval(()=>{
-  if(counter >= 0 && counter <= 500){
-    counter += 100;
-  }
-  else{
-    counter = 0;
-  }
-  image.style.transition = `1s`;
-  image.style.transform = `translate(${-counter}%)`
-},3000);
